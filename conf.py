2dc82db4af70ccffb2398f4a5a5b7b8e5afbd4e6
Aaron Jackson <aaron@aaronsplace.co.uk>
2024-02-06 21:48:59 +0000

    Test jquery fix

--- conf.py
+++ conf.py
@@ -144,6 +144,9 @@ html_logo = "img/logo.png"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+extensions = [
+    "sphinxcontrib.jquery",
+]
 
 def setup(app):
     app.add_css_file("custom.css")
